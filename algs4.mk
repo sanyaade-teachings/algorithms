7444c73b4c2b9c33f81520edf22ce321277524bb
Mark Pauley <pauley@anki.com>
2013-09-05 23:17:12 -0700

    More Makefile stuff

--- algs4.mk
+++ algs4.mk
@@ -1,24 +1,43 @@
+# master Makefile for Algorithms 4th Edition class
+# Author: Mark Pauley
+# no copyright, this file is yours to use as you wish!
+
 allObjects := $(assignmentObjects) $(otherObjects)
 assignmentJavaFiles = $(patsubst %,%.java,$(assignmentObjects))
 assignmentClassFiles = $(patsubst %,%.class,$(assignmentObjects))
-allJavaFiles = $(patsubst %,%.class,$(allObjects))
+allJavaFiles = $(patsubst %,%.java,$(allObjects))
 allClassFiles = $(patsubst %,%.class,$(allObjects))
+
+testJavaFiles = $(patsubst %,%.java,$(testObjects))
+testClassFiles = $(patsubst %,%.class,$(testObjects))
+
+classpath = ./:../stdlib.jar:../algs4.jar
+
+JAVA = java -classpath $(classpath)
+JAVAC = javac -classpath $(classpath)
+
+.PHONY: test checkstyle findbugs zip clean
 
 all: $(allClassFiles) checkstyle findbugs test
 
 %.class: %.java
-	javac -classpath ./:../stdlib.jar:../algs4.jar $<
+	$(JAVAC) $<
+
+%.test: %.class
+	$(JAVA) $(patsubst %.test,%,$@)
 
 checkstyle: $(assignmentJavaFiles)
-	java -classpath "./:../checkstyle-5.5/*" com.puppycrawl.tools.checkstyle.Main -c ../checkstyle-5.5/checkstyle.xml $(assignmentJavaFiles)
+	java -classpath "./:../checkstyle-5.5/*" com.puppycrawl.tools.checkstyle.Main -c ../checkstyle-5.5/checkstyle.xml $?
 
 findbugs: $(assignmentClassFiles)
-	../findbugs-2.0.1/bin/fb analyze -home ../findbugs-2.0.1/ -auxclasspath ./:../stdlib.jar:../algs4.jar $(assignmentClassFiles)
+	../findbugs-2.0.1/bin/fb analyze -home ../findbugs-2.0.1/ -auxclasspath $(classpath) $?
 
 zip: $(assignmentName).zip
 
 $(assignmentName).zip: checkstyle findbugs
 	zip $(assignmentName).zip Percolation.java PercolationStats.java
 
+test: $(patsubst %,%.test,$(testObjects))
+
 clean:
-	rm -f *.class $(assignmentName).zip *~ 
+	rm -f $(assignmentClassFiles) $(assignmentName).zip *~ 
