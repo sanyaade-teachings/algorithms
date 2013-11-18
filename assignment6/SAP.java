128cba134c1c8a2bbfcef013a688f4982dfa41af
Mark Pauley <pauley@anki.com>
2013-11-18 00:02:55 -0800

    SAP length works + tests

--- assignment6/SAP.java
+++ assignment6/SAP.java
@@ -12,15 +12,49 @@
  *************************************************************************/
 
 public class SAP {
-
+    private Digraph G;
+    private Digraph gRev;
+    
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
+        // O(E)
+        this.G = new Digraph(G);
 
+        // O(E)
+        gRev = this.G.reverse();
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return -1;
+        // 1) get a list of all ancestors of v
+        //  store this in ancestors. O(V + E)
+        ST<Integer, Integer> ancestors = new ST<Integer, Integer>();
+        Queue<Integer> q = new Queue<Integer>();
+        q.enqueue(v);
+        ancestors.put(v, 0);
+        while (!q.isEmpty()) {
+            int curAncestor = q.dequeue();
+            int ancestorDistance = ancestors.get(curAncestor);
+            for (int n : G.adj(curAncestor)) {
+                if (!ancestors.contains(n)) {
+                    ancestors.put(n, ancestorDistance + 1);
+                    q.enqueue(n);
+                }
+            }
+        }
+        
+        // 2) find the shortest path from rootPath to w (do BFS on G_rev)
+        //   O(V + E)
+        BreadthFirstDirectedPaths bfs 
+            = new BreadthFirstDirectedPaths(gRev, ancestors);
+        
+        if (!bfs.hasPathTo(w)) {
+            return -1;
+        }
+        else {
+            return bfs.distTo(w) 
+                + ancestors.get(bfs.pathTo(w).iterator().next());
+        }
     }
 
     // a common ancestor of v and w 
--- assignment6/SAPTest.java
+++ assignment6/SAPTest.java
@@ -11,9 +11,54 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class SAPTest {
+    Digraph testG;
+    SAP testSAP;
+
+    @Before public void createGraph() {
+        Digraph testG = new Digraph(20);
+        testG.addEdge(11, 10);
+        testG.addEdge(12, 10);
+        testG.addEdge(10, 5);
+        testG.addEdge(9, 5);
+        testG.addEdge(7, 3);
+        testG.addEdge(8, 3);
+        testG.addEdge(3, 1);
+        testG.addEdge(4, 1);
+        testG.addEdge(5, 1);
+        testG.addEdge(1, 0);
+        testG.addEdge(2, 0);
+        
+        // make a cycle (for testing)
+        testG.addEdge(18, 19);
+        testG.addEdge(19, 16);
+        testG.addEdge(16, 15);
+        testG.addEdge(15, 19);
+        testG.addEdge(15, 14);
+        testSAP = new SAP(testG);
+    }
 
     @Test public void testCreateSAP() {
+        assertNotNull(testSAP);
+    }
 
+    @Test public void testParent() {
+        assertEquals(1, testSAP.length(0, 2));
+    }
+
+    @Test public void testSameParent() {
+        assertEquals(2, testSAP.length(1, 2));
+    }
+
+    @Test public void testUncle() {
+        assertEquals(3, testSAP.length(9, 12));
+    }
+
+    @Test public void testDistantRelation() {
+        assertEquals(5, testSAP.length(7, 11));
+    }
+
+    @Test public void testReverseDistantRelation() {
+        assertEquals(5, testSAP.length(11, 7));
     }
 
     public static void main(String args[]) {
